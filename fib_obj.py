f531e75f1b5d08a4a1c143e1530049f760e8a761
liujian <506660782@qq.com>
2018-07-30 00:01:06 +0800

    经典的斐波那契数列

---
+++ fib_obj.py
@@ -0,0 +1,31 @@
+class Fib(object):
+    def __init__(self,max)
+    super(Fib,self).__init__()
+    self.max=max
+
+
+    def __iter__(self):
+        self.a=0
+        self.b=1
+        return self
+
+
+    def__next__(self):
+        fib=self.a
+        if fib>self.max:
+            raise StopIteration
+        self.a,self.b=self.b,self.a+self.b
+        return fib
+
+#定义一个main函数，循环遍历每一个斐波那契数列
+
+def main():
+    #20以内的数
+    fib=Fib(20)
+    for i in fib:
+        print(i)
+
+
+#测试
+if __name__=="__main__":
+    main()
